c8cbe438683fdd68c407cac140da4ceeb3139c75
Дияр Байсалов <baysalov.diyar@gmail.com>
2019-01-09 20:37:01 +0600

    add material-ui for components design
    Signed-off-by: Дияр Байсалов <baysalov.diyar@gmail.com>

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import React, { Component } from "react";
+import { hot } from "react-hot-loader";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Link,
+  Switch
+} from "react-router-dom";
+import Main from "./components/Main";
+import Signin from "./components/Signin";
+
+import "./Reset.css";
+import "./App.css";
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <div>
+//           <TopBar />
+//         </div>
+//         <div className="App">
+//           <h1> Hello, World!!</h1>
+//         </div>
+//       </React.Fragment>
+//     );
+//   }
+// }
+
+const fakeAuth = {
+  isAuthenticated: false,
+  authenticate(cb) {
+    this.isAuthenticated = true;
+    setTimeout(cb, 100); // fake async
+  },
+  signout(cb) {
+    this.isAuthenticated = false;
+    setTimeout(cb, 100);
+  }
+};
+
+function PrivateRoute({ component: Component, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        fakeAuth.isAuthenticated ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/signin",
+              state: { from: props.location }
+            }}
+          />
+        )
+      }
+    />
+  );
+}
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/signin/" component={Signin} />
+          <Route exact path="/" component={Main} />
+        </Switch>
+      </Router>
+    );
+  }
+}
+
+export default hot(module)(App);

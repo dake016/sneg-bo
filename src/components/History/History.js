6920c87164b26c09a02f7d7782f65c9e3876205e
Дияр Байсалов <baysalov.diyar@gmail.com>
2019-04-06 15:26:00 +0600

    new component -  history of actions, at working now
    Signed-off-by: Дияр Байсалов <baysalov.diyar@gmail.com>

---
+++ src/components/History/History.js
@@ -0,0 +1,82 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Tab from "@material-ui/core/Tab";
+import Tabs from "@material-ui/core/Tabs";
+import AuthService from "../Auth/AuthService";
+import withAuth from "../Auth/withAuth";
+
+const styles = theme => ({
+  secondaryBar: {
+    zIndex: 0
+  },
+  mainContent: {
+    flex: 1,
+    padding: "14px 36px 0",
+    background: "#eaeff1"
+  }
+});
+
+class History extends React.Component {
+  state = {
+    activeTab: 0,
+    history: []
+  };
+
+  Auth = new AuthService();
+
+  componentDidMount() {
+    this.getHistory();
+  }
+
+  getHistory() {
+    this.Auth.fetch(`${this.Auth.domain}/order/all`, { method: "GET" })
+      .then(response =>
+        this.setState({
+          history: response.content
+        })
+      )
+      .catch(error => alert("Orders " + error));
+  }
+
+  handleChange = (event, value) => {
+    this.setState({ activeTab: value });
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { activeTab } = this.state;
+
+    return (
+      <React.Fragment>
+        <AppBar
+          component="div"
+          className={classes.secondaryBar}
+          color="primary"
+          position="static"
+          elevation={0}
+        >
+          <Tabs
+            textColor="inherit"
+            value={activeTab}
+            onChange={this.handleChange}
+          >
+            <Tab textColor="inherit" label={"История действий Бэк-Офиса"} />
+            <Tab textColor="inherit" label={"История действий пользователей"} />
+          </Tabs>
+        </AppBar>
+        <main className={classes.mainContent}>
+          {activeTab === 0 && <React.Fragment />}
+          {activeTab === 1 && <OrdersTable />}
+        </main>
+      </React.Fragment>
+    );
+  }
+}
+
+History.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withAuth(withStyles(styles)(History));
